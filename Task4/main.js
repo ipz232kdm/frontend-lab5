6f7820fe507abb59f7ab1f2b9d44a9699f3c7e70
Kondratiuk Denys <ipz232_kdm@student.ztu.edu.ua>
2025-02-07 01:05:46 +0200

    Remove unused answerClick variable in createTask function

--- Task4/main.js
+++ Task4/main.js
@@ -1,74 +1,55 @@
 let numberInput = document.getElementById("numberInput");
 let score = document.getElementById("score");
 let correctAnswers = 0;
-
 let task = 0;
 
 let nextTaskButton = document.getElementById("nextTaskButton");
 let checkButton = document.getElementById("checkButton");
-
 let result = document.getElementById("result");
 
 function createTask() {
-    let num1 = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
-    let num2 = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
+    let num1 = Math.floor(Math.random() * 10) + 1;
+    let num2 = Math.floor(Math.random() * 10) + 1;
 
     document.getElementById("task").textContent = `${num1} x ${num2} = `;
 
     let product = num1 * num2;
 
-    answerClick = false;
-
     checkButton.onclick = () => {
-        if (numberInput.value !== ""){
-
+        if (numberInput.value !== "") {
             checkButton.disabled = true;
             numberInput.disabled = true;
 
-            if (numberInput.value === product.toString()){
+            if (parseInt(numberInput.value) === product) {
                 result.textContent = "Вірно!";
                 correctAnswers++;
+            } else {
+                result.textContent = `Помилка! Правильна відповідь "${product}"`;
             }
 
-            else result.textContent = `Помилка! Правильна відповідь "${product.toString()}"`;
-
             task++;
-
-            score.textContent = `Загальний рахунок ${correctAnswers*10}% (${correctAnswers} правильних відповідей з 10)`;
+            score.textContent = `Загальний рахунок ${correctAnswers * 10}% (${correctAnswers} правильних відповідей з 10)`;
+        } else {
+            alert("Введіть значення в текстове поле!");
         }
-
-        else (alert("Введіть значення в текстове поле!"));
-    }
+    };
 }
 
-nextTaskButton.onclick = () =>{
-    if (task === 10){
-        button.disabled = true;
+nextTaskButton.onclick = () => {
+    if (task === 10) {
+        nextTaskButton.disabled = true;
+        return;
     }
 
-    if (numberInput.disabled === true) {
-        // Очищуємо зайве та створюємо нове завдання
+    if (numberInput.disabled) {
         numberInput.value = "";
         numberInput.disabled = false;
-        result.textContent = '';
+        result.textContent = "";
         checkButton.disabled = false;
-
-        createTask(); // Генеруємо нове завдання
+        createTask();
+    } else {
+        alert("Перед тим як перейти до наступного завдання потрібно виконати це!");
     }
-
-    else {
-        alert("Перед тим як перейти до наступного завдання потрібно виконати це!")
-    }
-}
-
+};
 
 createTask();
-
-
-
-
-
-
-
-
-
